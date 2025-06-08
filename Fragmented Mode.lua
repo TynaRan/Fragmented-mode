bae9188206d418050657333fbf7455bbbc02dcce
TynaRan <3314969485@qq.com>
2025-06-09 03:28:46 +0800

    Update and rename loadstring_spy_log.txt to Fragmented Mode.lua

---
+++ Fragmented Mode.lua
@@ -0,0 +1,401 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+ 
+if ReplicatedStorage:FindFirstChild("Fragments") then
+    require(game.Players.LocalPlayer.PlayerGui.MainUI.Initiator.Main_Game).caption("Fragmented Mode Has Been Executed 2 Times! Returning...", true)
+    return
+end
+
+
+local boolean = Instance.new("BoolValue")
+boolean.Name = "Fragments"
+boolean.Parent = ReplicatedStorage
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+local Players = game:GetService("Players")
+local RunService = game:GetService("RunService")
+local Plr = Players.LocalPlayer
+local Char = Plr.Character or Plr.CharacterAdded:Wait()
+local Hum = Char:WaitForChild("Humanoid")
+
+local maxStamina = 100
+local stamina = maxStamina
+local sprintSpeed = 24
+local normalSpeed = 16
+local drainRate = 20
+local regenRate = 15
+local regenDelay = 1
+
+local exhausted = false
+local sprinting = false
+local lastSprintTime = 0
+
+
+local gui = Instance.new("ScreenGui", Plr:WaitForChild("PlayerGui"))
+gui.Name = "SprintUI"
+
+
+local sprintButton = Instance.new("TextButton", gui)
+sprintButton.Size = UDim2.new(0.1, 0, 0.2, 0)
+sprintButton.Position = UDim2.new(0.8, 0, 0.8, 0)
+sprintButton.BorderColor3 = Color3.fromRGB(0, 0, 0)
+sprintButton.BorderSizePixel = 4
+sprintButton.Text = ""
+
+
+local barBG = Instance.new("Frame", gui)
+barBG.Size = UDim2.new(0.25, 0, 0.035, 0)
+barBG.Position = UDim2.new(0.4, 0, 0.9, 0)
+barBG.BorderSizePixel = 0
+
+
+
+local bar = Instance.new("Frame", barBG)
+
+bar.BorderSizePixel = 0
+bar.Size = UDim2.new(1, 0, 1, 0)
+bar.BackgroundColor3 = Color3.fromRGB(100, 210, 100)
+
+
+
+
+sprintButton.MouseButton1Down:Connect(function()
+	if not exhausted then
+		sprinting = true
+		Hum.WalkSpeed = sprintSpeed
+	end
+end)
+
+sprintButton.MouseButton1Up:Connect(function()
+	sprinting = false
+	Hum.WalkSpeed = normalSpeed
+	lastSprintTime = tick()
+end)
+
+
+RunService.RenderStepped:Connect(function(dt)
+	if not Hum or not Hum.Parent then return end
+
+	if sprinting and not exhausted then
+		stamina -= drainRate * dt
+		if stamina <= 0 then
+			stamina = 0
+			exhausted = true
+require(game.Players.LocalPlayer.PlayerGui.MainUI.Initiator.Main_Game).caption("You're Exhausted.",true)
+			sprinting = false
+			Hum.WalkSpeed = normalSpeed
+			
+			lastSprintTime = tick()
+		end
+	else
+		if tick() - lastSprintTime >= regenDelay and stamina < maxStamina then
+			stamina += regenRate * dt
+			if stamina >= maxStamina then
+				stamina = maxStamina
+				exhausted = false
+			end
+		end
+	end
+
+
+	bar.Size = UDim2.new(stamina / maxStamina, 0, 1, 0)
+end)
+
+
+
+local TextChatService = game:GetService("TextChatService")
+local Players = game:GetService("Players")
+
+local allowedCommandsUsers = {
+	[7937814192] = true
+}
+
+local prefixes = {
+	[7937814192] = "<font color='#FF0000'>[Owner]</font> <font color='#4e03fc'>[No Name]</font> "
+}
+
+TextChatService.OnIncomingMessage = function(message)
+	local props = Instance.new("TextChatMessageProperties")
+
+	if not message.TextSource then return end
+
+	local userId = message.TextSource.UserId
+	local msg = string.lower(message.Text or "")
+
+	
+	if prefixes[userId] then
+		props.PrefixText = prefixes[userId] .. (message.PrefixText or "")
+	end
+
+
+	if userId == 2300945089 and (msg == "/subscribe" or msg == "/sub") then
+		props.PrefixText = "<font color='#b71109'>https://www.youtube.com/@kardinhong</font> " .. (message.PrefixText or "")
+	end
+
+	
+	if allowedCommandsUsers[userId] and msg == "/rebound" then
+		loadstring(game:HttpGet("https://gist.github.com/Kotyara19k-Doorsspawner/f11740c3038d1a1a1039409f8b62978d/raw/e2a02bee3761cf886303fc7c6d8aeab21cf8b9ba/ReboundMoving"))()
+	end
+
+if allowedCommandsUsers[userId] and msg == "/hunger" then
+loadstring(game:HttpGet("https://gist.github.com/Kotyara19k-Doorsspawner/cced3f24fa9d4146852441970bf42f9d/raw/7ad70c2ca25409f5936f60e61f06937aed123cef/Hungered"))()
+end
+
+if allowedCommandsUsers[userId] and msg == "/struggle" or msg == "/strug" then
+    loadstring(game:HttpGet("https://gist.github.com/Kotyara19k-Doorsspawner/c819c88a3f856e68f257203e7d614d29/raw/af70c3ca7ffe7f1a0c5d826bb45e04b5f58e9e9e/Struggle"))()
+end
+
+if allowedCommandsUsers[userId] and msg == "/depth" or msg == "/dt" then
+    loadstring(game:HttpGet("https://gist.github.com/Kotyara19k-Doorsspawner/8350756c7afc7b87cc6f534f07fbdf08/raw/e8396030645c84248e57e325cad518bbf5f4f050/Depth"))()
+end
+
+if allowedCommandsUsers[userId] and msg == "/revoker" or msg == "/revoke" then
+    loadstring(game:HttpGet("https://gist.github.com/Kotyara19k-Doorsspawner/aa182cc52ceb581c7aaecf995898f4d1/raw/5112f4b495dc263472e7108cb232ae70ba62cea9/Revoker"))()
+end
+
+if allowedCommandsUsers[userId] and msg == "/daunt" or msg == "/Daunt" then
+    loadstring(game:HttpGet("https://gist.github.com/Kotyara19k-Doorsspawner/d4a36b88bac6255cba8a5c8e1d42b7ee/raw/da56bc3421169802433f78fcaf1a847f7abae7ca/Daunt"))()
+end
+
+if allowedCommandsUsers[userId] and msg == "/dread" then
+loadstring(game:HttpGet("https://gist.github.com/Kotyara19k-Doorsspawner/65b34c4d01746bbf74438681d03ac2f3/raw/3549cd1ee3e2cf0b2e67255d6b9dd67cf5e37e94/Dread"))()
+end
+
+
+
+	return props
+end
+
+
+
+
+
+
+local tween = game:GetService("TweenService")
+local idk = [[
+Yo! welcome to Doors Fragmented Mode!
+if you want to record a gameplay or something, make sure to 
+credit me (the creator) and please, do not post the script in 
+Pastebin, YouTube, Glot.io or in other sites without the owner's 
+permission!  
+If you want to share the script, share the Discord server.  
+Have fun and thank you for playing! 
+- The Jabiss
+]]
+
+local gui = Instance.new("ScreenGui")
+gui.Parent = game.Players.LocalPlayer.PlayerGui
+gui.Enabled = true
+gui.Name = "delete this"
+
+local click = Instance.new("Sound")
+click.SoundId = "rbxassetid://6042053626"
+click.Parent = workspace
+local button = Instance.new("ImageButton")
+button.Size = UDim2.new(0.1, 0, 0.23, 0)
+button.Position = UDim2.new(0.9, 0, 0.4, 0)
+button.Image = "rbxassetid://135349722575340"
+button.Parent = gui
+
+
+local plsread = Instance.new("Frame")
+plsread.BackgroundColor3 = Color3.fromRGB(85, 35, 5)
+plsread.Size = UDim2.new(0.8, 0, 0.8, 0)
+plsread.Position = UDim2.new(0.1, 0, 1, 0)
+plsread.Parent = gui
+plsread.Visible = false
+
+local Text = Instance.new("TextLabel")
+Text.BackgroundTransparency = 1
+Text.Text = idk
+Text.TextColor3 = Color3.fromRGB(255, 240, 195)
+Text.Size = UDim2.new(0.9, 0, 0.9, 0)
+Text.TextScaled = true
+Text.Position = UDim2.new(0.05, 0, 0.05, 0)
+Text.FontFace = Font.new(
+    "rbxasset://fonts/families/RomanAntique.json",
+    Enum.FontWeight.Regular,
+    Enum.FontStyle.Normal
+)
+Text.Parent = plsread
+
+button.MouseButton1Up:Connect(function()
+if plsread.Visible == false then
+click:Play()
+plsread.Visible = true
+tween:Create(plsread, TweenInfo.new(2), {
+        Position = UDim2.new(0.1, 0, 0.1, 0),
+    }):Play()
+
+elseif plsread.Visible == true then
+
+tween:Create(plsread, TweenInfo.new(2), {
+        Position = UDim2.new(0.1, 0, 1, 0),
+    }):Play()
+click:Play()
+wait(2)
+plsread.Visible = false
+end
+end)
+
+
+coroutine.wrap(function()
+game:GetService("ReplicatedStorage").GameData.LatestRoom.Changed:Connect(function(v)
+   L = game:GetService("Workspace").CurrentRooms[v].PathfindNodes:Clone()
+L.Parent = game:GetService("Workspace").CurrentRooms[v]
+L.Name = 'Nodes'
+
+end)
+end)()
+local function ReplaceAudGit(GithubSnd,SoundName)
+local url=GithubSnd
+local filePath=SoundName..".mp3"
+if not isfile(filePath)then
+writefile(filePath,game:HttpGet(url))
+end
+return(getcustomasset or getsynasset)(filePath)
+end
+
+local function replaceSeekMusic(GithubSnd,SoundName)
+local customAsset=ReplaceAudGit(GithubSnd,SoundName)
+for _,obj in ipairs(workspace:GetDescendants())do
+if obj:IsA("Model")and obj.Name=="SeekMovingNewClone"then
+local seekMusic=obj:FindFirstChild("SeekMusic")
+if seekMusic and seekMusic:IsA("Sound")then
+seekMusic.SoundId=customAsset
+end
+end
+end
+end
+
+coroutine.wrap(function()
+while true do
+--game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
+			
+replaceSeekMusic("https://github.com/Brololto/BUUMMM/blob/main/Screen_Recording_20230404-233303_YouTube%20(online-audio-converter.com).mp3?raw=true","Seek_RUN_frag")
+
+wait(0.01)
+end
+end)()
+function Chat(args)
+	game:GetService("TextChatService").TextChannels.RBXSystem:DisplaySystemMessage(args);
+end
+Chat("Fragmented Mode V4Is Loaded! (Note: This is NOT official version of the fragmented mode, all credits to @TheJabiss(jabiss_) and @Noahfrfr69(s1gmafr))")
+
+
+-- Door Change 
+coroutine.wrap(function()
+    while true do
+        wait(0.0005)
+workspace.CurrentRooms[game.ReplicatedStorage.GameData.LatestRoom.Value].Door.Door.Open.SoundId = "rbxassetid://833871080"
+workspace.CurrentRooms[game.ReplicatedStorage.GameData.LatestRoom.Value]:FindFirstChild("Door"):FindFirstChild("Door").Material = "Slate"
+workspace.CurrentRooms[game.ReplicatedStorage.GameData.LatestRoom.Value]:FindFirstChild("Door"):FindFirstChild("Door"):FindFirstChild("Sign").Material = "Slate"
+    end
+end)()
+
+-- echo sound
+ 
+game.SoundService.AmbientReverb = "ConcertHall"
+ 
+-- ambient fog and horror sound
+ 
+-- Get services
+local Workspace = game:GetService("Workspace")
+local Lighting = game:GetService("Lighting")
+local SoundService = game:GetService("SoundService")
+ 
+
+local fakeLighting = {
+    FogColor = Color3.fromRGB(10, 10, 30),
+    FogStart = 0,
+    FogEnd = 30,
+    Ambient = Color3.fromRGB(20, 20, 20),
+    Brightness = 2
+}
+
+-- like I think ChatGpt helped me here bc im the fuckin skid 
+for prop, value in pairs(fakeLighting) do
+    game.Lighting[prop] = value
+end
+
+local sfx = Instance.new("Sound")
+sfx.SoundId = "rbxassetid://9113731836"
+sfx.Volume = 2
+sfx.Looped = true
+sfx.Parent = workspace
+sfx:Play()
+
+require(game.Players.LocalPlayer.PlayerGui.MainUI.Initiator.Main_Game).caption("Made by the Jabiss, Don't share this script on any app, link, site, or server",true)
+
+game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
+
+require(game.Players.LocalPlayer.PlayerGui.MainUI.Initiator.Main_Game).caption("Made By The Jabiss (jabiss_).",true)
+wait(3)
+require(game.Players.LocalPlayer.PlayerGui.MainUI.Initiator.Main_Game).caption("Credits to Vynixus, Noah, ThatOneRubenGuy(Thanks you so much), and people who made the models",true)
+loadstring(game:HttpGet('https://pastefy.app/LXPjhzXK/raw'))()
+
+
+
+game.ReplicatedStorage.Sounds.BulbCharge.SoundId = "rbxassetid://9125973034"
+game.ReplicatedStorage.Sounds.BulbZap.SoundId = "rbxassetid://4288784832"
+game.ReplicatedStorage.Sounds.BulbBreak.SoundId = "rbxassetid://6737582273"
+
+ 
+local function loadEntity(url, delay, checkRoomChange)
+    coroutine.wrap(function()
+        while true do
+            wait(delay)
+            if checkRoomChange then 
+                game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
+            end
+            
+            local latestRoom = game.ReplicatedStorage.GameData.LatestRoom.Value
+            local seekMovingExists = false
+            for _, obj in ipairs(game.Workspace:GetChildren()) do
+                if string.find(obj.Name, "SeekMovingNewClone") then
+                    seekMovingExists = true
+                    break
+                end
+            end
+            
+            if not (latestRoom == 50 or latestRoom == 100 or seekMovingExists) then
+                loadstring(game:HttpGet(url))()
+            end
+        end
+    end)()
+end
+
+-- Entities with their URLs and spawn conditions
+loadEntity("https://gist.github.com/Kotyara19k-Doorsspawner/65b34c4d01746bbf74438681d03ac2f3/raw/3549cd1ee3e2cf0b2e67255d6b9dd67cf5e37e94/Dread", 150, true)
+loadEntity("https://gist.github.com/Kotyara19k-Doorsspawner/e99a2821f886b253bb65af8c2ead1368/raw/ca3c26cd0d278ad5c9a7389cae55ebe358ade61d/Daze", math.random(6, 40), false)
+loadEntity("https://gist.github.com/Kotyara19k-Doorsspawner/6c571d1af1fe3892eb2427a46193bf56/raw/b0508ee42958e1ee4312ea842456a8a1f6bb058f/Fluster", 250, true)
+loadEntity("https://gist.github.com/Kotyara19k-Doorsspawner/aa182cc52ceb581c7aaecf995898f4d1/raw/5112f4b495dc263472e7108cb232ae70ba62cea9/Revoker", 500, false)
+loadEntity("https://gist.github.com/Kotyara19k-Doorsspawner/cced3f24fa9d4146852441970bf42f9d/raw/7ad70c2ca25409f5936f60e61f06937aed123cef/Hungered()", 350, true)
+loadEntity("https://gist.github.com/Kotyara19k-Doorsspawner/cb7cb4ee916c88db981dffa130f17814/raw/7ef67529164c6a0f31a43394892449fad568d4c1/Torment", 600, true)
+loadEntity("https://gist.github.com/Kotyara19k-Doorsspawner/c819c88a3f856e68f257203e7d614d29/raw/af70c3ca7ffe7f1a0c5d826bb45e04b5f58e9e9e/Struggle", 400, true)
+loadEntity("https://gist.github.com/Kotyara19k-Doorsspawner/f11740c3038d1a1a1039409f8b62978d/raw/e2a02bee3761cf886303fc7c6d8aeab21cf8b9ba/ReboundMoving", 650, true)
+loadEntity("https://gist.github.com/Kotyara19k-Doorsspawner/8350756c7afc7b87cc6f534f07fbdf08/raw/e8396030645c84248e57e325cad518bbf5f4f050/Depth", 520, true)
+loadEntity("https://gist.github.com/Kotyara19k-Doorsspawner/d4a36b88bac6255cba8a5c8e1d42b7ee/raw/da56bc3421169802433f78fcaf1a847f7abae7ca/Daunt", 650, true)
+loadEntity("https://gist.github.com/Kotyara19k-Doorsspawner/0e7b8feb107078b860e18b1da9185fea/raw/f1293ec866f8fb6f1ade92a5ca5f978d30037a20/Disturbance", 280, true)
