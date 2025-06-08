3681c56b6d502046c414356e44461978a45b1233
TynaRan <3314969485@qq.com>
2025-06-09 04:28:51 +0800

    Update Torment-Fixed.lua

--- Torment-Fixed.lua
+++ Torment-Fixed.lua
@@ -54,13 +54,30 @@ Cause = ""
 }
 })
 entity:SetCallback("OnSpawned", function()
-print("Entity has spawned")
 function GitAud(soundgit,filename)
 local url=soundgit
 local FileName = filename
 writefile(FileName..".mp3", game:HttpGet(url))
 return (getcustomasset or getsynasset)(FileName..".mp3")
 end
+
+function CustomGitSound(soundlink, vol, filename)
+if not workspace:FindFirstChild("Torment") then return end
+local Sound = Instance.new("Sound")
+Sound.SoundId = GitAud(soundlink, filename)
+Sound.Parent = workspace
+Sound.Volume = vol
+Sound:Play()
+
+coroutine.wrap(function()
+while workspace:FindFirstChild("Torment") do
+wait(1)
+end
+Sound:Stop()
+Sound:Destroy()
+end)()
+end
+
 CustomGitSound("https://github.com/Kotyara19k-Doorsspawner/Random-files/raw/main/StepsNLooks.mp3", 1, "StepForStepIGotYourDataFiles")
 end)
 entity:SetCallback("OnStartMoving", function()
@@ -90,30 +107,30 @@ end)()
 end)
 entity:SetCallback("OnEnterRoom", function(room, firstTime)
 if firstTime == true then
-print("Entity has entered room: ".. room.Name.. " for the first time")
+--print("Entity has entered room: ".. room.Name.. " for the first time")
 else
-print("Entity has entered room: ".. room.Name.. " again")
+--print("Entity has entered room: ".. room.Name.. " again")
 end
 end)
 entity:SetCallback("OnLookAt", function(lineOfSight)
 if lineOfSight == true then
-print("Player is looking at entity")
+--print("Player is looking at entity")
 else
-print("Player view is obstructed by something")
+--print("Player view is obstructed by something")
 end
 end)
 entity:SetCallback("OnRebounding", function(startOfRebound)
 if startOfRebound == true then
-print("Entity has started rebounding")
+--print("Entity has started rebounding")
 else
-print("Entity has finished rebounding")
+--print("Entity has finished rebounding")
 end
 end)
 entity:SetCallback("OnDespawning", function()
-print("Entity is despawning")
+--print("Entity is despawning")
 end)
 entity:SetCallback("OnDespawned", function()
-print("Entity has despawned")
+--print("Entity has despawned")
 end)
 entity:SetCallback("OnDamagePlayer", function(newHealth)
 if newHealth == 0 then
